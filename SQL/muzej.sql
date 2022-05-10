7b9e0753cf48c7b987231db51fda9d29cb6df117
Filip Kovac <filip.kvch@gmail.com>
2022-05-10 21:09:41 +0200

    Dodano ubacivanje u tablicu

--- SQL/muzej.sql
+++ SQL/muzej.sql
@@ -2,27 +2,68 @@
 
 # Muzej
 
+-- U muzeju postoji više izložaba. 
+-- Jedna izložba ima više djela. 
+-- Svaki kustos je zadužen za jednu izložbu. 
+-- Svaka izložba ima jednog sponzora.
+
 drop database if exists muzej;
 create database muzej;
 use muzej;
 
 create table izlozba (
+    sifra int not null primary key auto_increment,
+    vrsta_izlozbe varchar(50) not null,
+    broj_djela int,
+    sponzor int not null
+);
+
+create table djelo (
+    sifra int not null primary key auto_increment,
+    naziv_djela varchar(50) not null,
     vrsta_djela varchar(50),
-    broj_djela varchar(50),
-    kustos varchar(50),
-    sponzor varchar(50)
+    izlozba int not null
 );
 
 create table kustos (
-    ime varchar(50),
-    prezime varchar(50),
-    OIB varchar(50),
+    sifra int not null primary key auto_increment,
+    ime varchar(50) not null,
+    prezime varchar(50) not null,
+    OIB char(11),
     IBAN varchar(50),
-    email varchar(50)
+    email varchar(50),
+    izlozba int not null
 );
 
 create table sponzor (
-    ime_sponzora varchar(50),
+    sifra int not null primary key auto_increment,
+    ime_sponzora varchar(50) not null,
     vrijednost_sponzora varchar(50),
     trajanje_ugovora varchar(50)
 );
+
+alter table izlozba add foreign key (sponzor) references sponzor(sifra);
+alter table kustos add foreign key (izlozba) references izlozba(sifra);
+alter table djelo add foreign key (izlozba) references izlozba(sifra);
+
+# Ubacivanje
+
+-- select * from izlozba;
+
+-- insert into izlozba (vrsta_izlozbe, sponzor)
+-- values ('Slike', 1);
+
+-- select * from sponzor;
+
+-- insert into sponzor (ime_sponzora)
+-- values ('Pevec');
+
+-- select * from djelo;
+
+-- insert into djelo (naziv_djela, izlozba)
+-- values ('Kikiriki', 1);
+
+-- select * from kustos;
+
+-- insert into kustos (ime, prezime, izlozba)
+-- values ('Filip', 'Kovac', 1);
