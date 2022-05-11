48267726723668443b878ce0163e593d2856bdfb
Filip Kovac <filip.kvch@gmail.com>
2022-05-11 16:30:41 +0200

    Dodane veze
    I popravljeni atributi i entiteti

--- SQL/taxi_sluzba.sql
+++ SQL/taxi_sluzba.sql
@@ -2,32 +2,63 @@
 
 # Taxi služba
 
+-- Taksi tvrtka ima više vozila. 
+-- Jedno vozilo vozi jedan vozač. 
+-- U jednoj vožnji vozač može prevesti više putnika. 
+-- Jedan putnik se naravno može tijekom vremena imati više vožnji.
+
 drop database if exists taxi_sluzba;
 create database taxi_sluzba;
 use taxi_sluzba;
 
 create table vozilo (
-    proizvodjac varchar(50),
-    vrsta_vozila varchar(50),
-    prijedjeni_kilometri varchar(50),
-    maksimalni_broj_putnika varchar(50)
+    sifra int not null primary key auto_increment,
+    proizvodjac varchar(30),
+    vrsta_vozila varchar(30),
+    prijedjeni_kilometri decimal,
+    maksimalni_broj_putnika int not null,
+    vozac int not null
 );
 
 create table vozac (
-    osoba varchar(50),
+    sifra int not null primary key auto_increment,
+    osoba int not null,
     IBAN varchar(50),
-    vozilo varchar(50)
+    vozilo int not null,
+    voznja int not null
 );
 
 create table putnik (
-    osoba varchar(50),
+    sifra int not null primary key auto_increment,
+    osoba int not null,
+    putnik_voznja int not null
+);
+
+create table putnik_voznja (
+    sifra int not null primary key auto_increment,
+    putnik int not null,
+    voznja int not null
+);
+
+create table voznja (
+    sifra int not null primary key auto_increment,
     polaziste varchar(50),
-    odrediste varchar(50)
+    odrediste varchar(50),
+    cijena decimal,
+    putnik_voznja int not null
 );
 
 create table osoba (
-    ime varchar(50),
-    prezime varchar(50),
-    OIB varchar(50),
+    sifra int not null primary key auto_increment,
+    ime varchar(30),
+    prezime varchar(30),
+    OIB char(11),
     email varchar(50)
 );
+
+alter table vozac add foreign key (osoba) references osoba(sifra);
+alter table vozac add foreign key (vozilo) references vozilo(sifra);
+alter table vozac add foreign key (voznja) references voznja(sifra);
+
+alter table putnik_voznja add foreign key (putnik) references putnik(sifra);
+alter table putnik_voznja add foreign key (voznja) references voznja(sifra);
