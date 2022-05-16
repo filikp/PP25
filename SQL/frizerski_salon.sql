f444714b1cf0609172d55115f6ce0ca3e7e5e8e1
Filip Kovac <filip.kvch@gmail.com>
2022-05-16 15:42:09 +0200

    Unesene neke vrijednosti

--- SQL/frizerski_salon.sql
+++ SQL/frizerski_salon.sql
@@ -1,4 +1,4 @@
-# C:\xampp\mysql\bin\mysql -uroot < C:\Users\Kleep\Documents\GitHub\PP25\SQL\frizerski_salon.sql
+# C:\xampp\mysql\bin\mysql -uroot --default_character_set=utf8 < C:\Users\Kleep\Documents\GitHub\PP25\SQL\frizerski_salon.sql
 
 # Frizerski salon
 
@@ -7,7 +7,7 @@
 -- Korisnik tijekom jednog posjeta koristi jednu usluga.
 
 drop database if exists frizerski_salon;
-create database frizerski_salon;
+create database frizerski_salon charset utf8mb4;
 use frizerski_salon;
 
 create table djelatnik (
@@ -40,8 +40,30 @@ create table usluga (
     trajanje_minute int
 );
 
-alter table korisnik add foreign key (djelatnik) references djelatnik(sifra);
-alter table korisnik add foreign key (usluga) references usluga(sifra);
-alter table korisnik add foreign key (osoba) references osoba(sifra);
+alter table korisnik add foreign key (djelatnik) references djelatnik(sifra) ON DELETE CASCADE;
+alter table korisnik add foreign key (usluga) references usluga(sifra) ON DELETE CASCADE;
+alter table korisnik add foreign key (osoba) references osoba(sifra) ON DELETE CASCADE;
 
 alter table djelatnik add foreign key (osoba) references osoba(sifra);
+
+INSERT INTO osoba (ime, prezime)
+VALUES  ('Filip', 'Kovač'),
+        ('Mirko', 'Filipović'),
+        ('Janica', 'Kostelić'),
+        ('Đurđa', 'Botušić');
+
+INSERT INTO djelatnik (osoba)
+VALUES  (3),
+        (4);
+
+INSERT INTO usluga (naziv_usluge, cijena)
+VALUES  ('šišanje kose', 50),
+        ('brijanje brade', 30),
+        ('bojanje kose', 100),
+        ('pranje kose', 20);
+
+INSERT INTO korisnik (osoba, usluga, student, djelatnik)
+VALUES  (1,1,1,1),
+        (2,3,0,2);
+
+        
