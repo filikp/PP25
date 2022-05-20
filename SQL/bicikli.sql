39a98fd6589424d15477c8a8429c676d0e52a430
Filip Kovac <filip.kvch@gmail.com>
2022-05-20 08:44:05 +0200

    Neke tablice izbačene, račun ubačen
    Izbačene tablice: velicina, broj_brzina, brzine_u_vrsti, osoba, trgovina, lokacija, bicikl_trgovina.
    Ubačen: racun

--- SQL/bicikli.sql
+++ SQL/bicikli.sql
@@ -7,85 +7,36 @@ use trgovina_bicikala;
 create table bicikl (
     sifra int not null primary key auto_increment,
     proizvodac varchar(50) not null,
-    velicina int not null,
-    vrsta int not null
-);
-
-create table velicina (
-    sifra int not null primary key auto_increment,
-    velicina_rame decimal(5,1),
-    velicina_tockova decimal(5,1)
-);
-
-create table vrsta (
-    sifra int not null primary key auto_increment,
     namjena varchar(40),
-    elektricni boolean
-);
-
-create table broj_brzina (
-    sifra int not null primary key auto_increment,
-    prednji_zupcanik int,
-    straznji_zupcanik int
-); 
-
-create table brzine_u_vrsti (
-    sifra int not null primary key auto_increment,
-    broj_brzina int not null,
-    vrsta int not null
-);
-
-create table osoba (
-    sifra int not null primary key auto_increment,
-    ime varchar(50) not null,
-    prezime varchar(50) not null,
-    email varchar(50),
-    OIB char(11)
+    elektricni boolean,
+    broj_brzina int,
+    velicina decimal(5,2),
+    racun int not null
 );
 
 create table prodavac (
     sifra int not null primary key auto_increment,
-    osoba int not null,
-    IBAN varchar(50),
-    trgovina int not null
+    ime varchar(50) not null,
+    prezime varchar(50) not null,
+    email varchar(50),
+    OIB char(11),
+    IBAN varchar(50)
 );
 
 create table kupac (
     sifra int not null primary key auto_increment,
-    osoba int not null,
-    bicikl int not null
-);
-
-create table trgovina (
-    sifra int not null primary key auto_increment,
     ime varchar(50) not null,
+    prezime varchar(50) not null,
     bicikl int not null
 );
 
-create table lokacija (
+create table racun (
     sifra int not null primary key auto_increment,
-    grad varchar(50) not null,
-    postanski_broj int not null,
-    ulica varchar(50) not null,
-    kucni_broj int not null,
-    trgovina int not null
+    vrijeme_kupnje datetime,
+    prodavac int not null,
+    kupac int not null
 );
 
-create table bicikl_trgovina (
-    sifra int not null primary key auto_increment,
-    bicikl int not null,
-    trgovina int not null,
-    cijena decimal (18,2)
-);
-
-
-alter table bicikl add foreign key (velicina) references velicina(sifra);
-alter table bicikl add foreign key (vrsta) references vrsta(sifra);
-alter table lokacija add foreign key (trgovina) references trgovina(sifra);
-alter table bicikl_trgovina add foreign key (trgovina) references trgovina(sifra);
-alter table bicikl_trgovina add foreign key (bicikl) references bicikl(sifra);
-alter table prodavac add foreign key (trgovina) references trgovina(sifra);
-alter table prodavac add foreign key (osoba) references osoba(sifra);
-alter table kupac add foreign key (osoba) references osoba(sifra);
-alter table brzine_u_vrsti add foreign key (broj_brzina) references broj_brzina(sifra);
-alter table brzine_u_vrsti add foreign key (vrsta) references vrsta(sifra);
+alter table bicikl add foreign key (racun) references racun(sifra);
+alter table racun add foreign key (prodavac) references prodavac(sifra);
+alter table racun add foreign key (kupac) references kupac(sifra);
