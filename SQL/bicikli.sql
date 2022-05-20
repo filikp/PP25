365c3acc2ad545c928502a54ac1fb103c06826dc
Filip Kovac <filip.kvch@gmail.com>
2022-05-20 08:52:32 +0200

    Dodana tablica Trgovina

--- SQL/bicikli.sql
+++ SQL/bicikli.sql
@@ -11,7 +11,16 @@ create table bicikl (
     elektricni boolean,
     broj_brzina int,
     velicina decimal(5,2),
-    racun int not null
+    racun int not null,
+    trgovina int not null
+);
+
+create table trgovina (
+    sifra int not null primary key auto_increment,
+    ime varchar(50),
+    OIB char(11),
+    email varchar(50),
+    telefon varchar(30)
 );
 
 create table prodavac (
@@ -20,7 +29,8 @@ create table prodavac (
     prezime varchar(50) not null,
     email varchar(50),
     OIB char(11),
-    IBAN varchar(50)
+    IBAN varchar(50),
+    trgovina int not null
 );
 
 create table kupac (
@@ -34,9 +44,13 @@ create table racun (
     sifra int not null primary key auto_increment,
     vrijeme_kupnje datetime,
     prodavac int not null,
-    kupac int not null
+    kupac int not null,
+    trgovina int not null
 );
 
 alter table bicikl add foreign key (racun) references racun(sifra);
 alter table racun add foreign key (prodavac) references prodavac(sifra);
 alter table racun add foreign key (kupac) references kupac(sifra);
+alter table bicikl add foreign key (trgovina) references trgovina(sifra);
+alter table racun add foreign key (trgovina) references trgovina(sifra);
+alter table prodavac add foreign key (trgovina) references trgovina(sifra);
