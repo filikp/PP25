8e33ee911e4a408bca1cbd20c5ac0052a9488e81
Filip Kovac <filip.kvch@gmail.com>
2022-05-22 22:44:17 +0200

    Korekcije

--- SQL/bicikli.sql
+++ SQL/bicikli.sql
@@ -11,16 +11,7 @@ create table bicikl (
     elektricni boolean,
     broj_brzina int,
     velicina decimal(5,2),
-    racun int not null,
-    trgovina int not null
-);
-
-create table trgovina (
-    sifra int not null primary key auto_increment,
-    ime varchar(50),
-    OIB char(11),
-    email varchar(50),
-    telefon varchar(30)
+    cijena decimal(18,2)
 );
 
 create table prodavac (
@@ -29,28 +20,30 @@ create table prodavac (
     prezime varchar(50) not null,
     email varchar(50),
     OIB char(11),
-    IBAN varchar(50),
-    trgovina int not null
+    IBAN varchar(50)
 );
 
 create table kupac (
     sifra int not null primary key auto_increment,
     ime varchar(50) not null,
-    prezime varchar(50) not null,
-    bicikl int not null
+    prezime varchar(50) not null
+);
+
+create table stavka (
+    sifra int not null primary key auto_increment,
+    bicikl int not null,
+    kolicina int,
+    racun int not null
 );
 
 create table racun (
     sifra int not null primary key auto_increment,
     vrijeme_kupnje datetime,
     prodavac int not null,
-    kupac int not null,
-    trgovina int not null
+    kupac int not null
 );
 
-alter table bicikl add foreign key (racun) references racun(sifra);
+alter table stavka add foreign key (racun) references racun(sifra);
+alter table stavka add foreign key (bicikl) references bicikl(sifra);
 alter table racun add foreign key (prodavac) references prodavac(sifra);
 alter table racun add foreign key (kupac) references kupac(sifra);
-alter table bicikl add foreign key (trgovina) references trgovina(sifra);
-alter table racun add foreign key (trgovina) references trgovina(sifra);
-alter table prodavac add foreign key (trgovina) references trgovina(sifra);
