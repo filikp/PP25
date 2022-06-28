9d8911286248e427b3efa2c345bccc53e95e157a
Filip Kovac <filip.kvch@gmail.com>
2022-06-28 19:09:14 +0200

    Potrebno za 6. zadatak

---
+++ www/ucenjephp.hr/izbornik.php
@@ -0,0 +1,55 @@
+<!-- Start izbornik -->
+<div class="title-bar" data-responsive-toggle="example-animated-menu" data-hide-for="medium">
+      <button class="menu-icon" type="button" data-toggle></button>
+      <div class="title-bar-title">Učenje PHP-a</div>
+    </div>
+
+    <div class="top-bar" id="example-animated-menu" data-animate="hinge-in-from-top spin-out">
+      <div class="top-bar-left">
+        <ul class="dropdown menu" data-dropdown-menu>
+          <li class="menu-text">Učenje PHP-a</li>
+          <li><a href="index.php">Početna</a></li>
+          <li>
+            <a href="#">Osnove</a>
+            <ul class="menu vertical">
+              <li><a href="grid.php">Grid sustav</a></li>
+              <li><a href="prijenosParametara.php?ime=Edunova">Prijenos parametara</a></li>
+              <li><a href="prijenosDvaParametra.php?boja=red&ime=Osijek">Prijenos dva parametra</a></li>
+            </ul>
+          </li>
+          <li>
+            <a href="#">Osnove PHP-a</a>
+            <ul class="menu vertical">
+                <li>
+                    <a href="#">Varijabla i tipovi podataka</a>
+                    <ul class="menu vertical">
+                        <li><a target="_blank" href="osnovephp/varijabla.php">Varijabla</a></li>
+                        <li><a target="_blank" href="osnovephp/operatori.php">Operatori</a></li>
+                        <li><a target="_blank" href="osnovephp/prviProgram.php">Prvi program</a></li>
+                        <li><a target="_blank" href="vizualniProgram.php">Vizualni program</a></li>
+                        <li><a target="_blank" href="uvjetnoGrananjeIf.php">Uvjetno grananje If</a></li>
+                        <li><a target="_blank" href="uvjetnoGrananjeSwitch.php">Uvjetno grananje Switch</a></li>
+                    </ul>
+                </li>
+            </ul>
+          </li>
+          <li>
+            <a href="#">Zadaci</a>
+            <ul class="menu vertical">
+                <li>
+                    <a href="#">Osnove</a>
+                    <ul class="menu vertical">
+                        <li><a href="Z01.php">Zadatak 1</a></li>
+                        <li><a href="Z02.php">Zadatak 2</a></li>
+                        <li><a href="Z03.php">Zadatak 3</a></li>
+                        <li><a href="Z04.php">Zadatak 4</a></li>
+                        <li><a href="Z05.php">Zadatak 5</a></li>
+                        <li><a href="Z06.php">Zadatak 6</a></li>
+                    </ul>
+                </li>
+            </ul>
+          </li>
+        </ul>
+      </div>
+    </div>
+    <!-- End izbornik -->
---
+++ www/ucenjephp.hr/podnozje.php
@@ -0,0 +1,9 @@
+<!-- Start podnožje -->
+<div class="grid-x">
+      <div class="cell">
+        <div class="callout">
+          &copy; Edunova 2022
+        </div>
+      </div>
+    </div>
+    <!-- End podnožje -->
