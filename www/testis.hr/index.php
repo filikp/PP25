8ce4fe678bf31aec7d335fc2bee84f282a5cdb4c
Filip Kovac <filip.kvch@gmail.com>
2022-07-27 12:40:06 +0200

    Vježbanje - button

---
+++ www/testis.hr/index.php
@@ -0,0 +1,17 @@
+<html lang="en">
+<head>
+    <title>My HTML Document</title>
+    <style>
+        body { background-color: YellowGreen; }
+        p { color: #00ffff; }
+    </style>
+</head>
+<body>
+    <button>
+        Click
+    </button>
+    <br>
+    <button>Filip</button>
+    <button>mrdavac</button>
+</body>
+</html>
