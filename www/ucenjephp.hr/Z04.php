4a94c821c1f9f91e999ffaaef5ae32c21ffa4163
Filip Kovac <filip.kvch@gmail.com>
2022-09-07 14:25:01 +0200

    Update Z04.php

--- www/ucenjephp.hr/Z04.php
+++ www/ucenjephp.hr/Z04.php
@@ -1,6 +1,6 @@
 <?php
 
-//Stranica prima 4 parametra i ispisuje umnožak zbroja prvi i drugi te teći i četvrti
+//Stranica prima 4 parametra i ispisuje umnožak zbroja prvi i drugi te treći i četvrti
 
 //pr
 //ulaz: 2 2 1 3
@@ -36,7 +36,7 @@ $p4 = isset($_GET['p4']) ? $_GET['p4'] : 0;
             Unesi cetvrti broj:
             <input type="text" name="p4" value="<?=$p4?>">
         </div>
-        <div>
+        <div class="cell large-6 medium-6">
             <input class="success button expanded" type="submit" value="Izračunaj">
             <a class="alert button expanded" href="Z04.php">RESET</a>
         </div>
@@ -47,5 +47,6 @@ $p4 = isset($_GET['p4']) ? $_GET['p4'] : 0;
 <?php
 
 $rez = ($p1 + $p2) * ($p3 + $p4);
-echo '('.$p1.'+'.$p2.')*('.$p3.'+'.$p4.') = '.$p1+$p2 . '*' . $p3+$p4 . ' = '. $rez;
+echo 'Rezultat je: ' . $rez, '<br>';
+echo 'Koraci: ('.$p1.'+'.$p2.')*('.$p3.'+'.$p4.') = '.$p1+$p2 . '*' . $p3+$p4 . ' = '. $rez;
 ?>
